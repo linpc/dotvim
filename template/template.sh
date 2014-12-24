5746916b821e044386bafc6dc04a5d3a5cecb29b
Po-Chien Lin <linpc@cs.nctu.edu.tw>
2014-12-24 20:19:19 +0800

    Add option handler to shell template

--- template/template.sh
+++ template/template.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
+opt_handle()
+{
+    while getopts h op
+    do
+        case ${op} in
+            *|h)
+                exit 0
+                ;;
+            esac
+    done
+}
+
 main()
 {
     echo $@
+
+    opt_handle $@
+    shift $((${OPTIND} - 1))
 }
 
 main $@
